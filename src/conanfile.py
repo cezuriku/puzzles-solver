799d5013ddb844f0042015d09d11e70b9defaf77
cezuriku <cezuriku@users.noreply.github.com>
2024-08-15 10:04:59 +0200

    Initial commit

---
+++ src/conanfile.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-class-docstring, too-few-public-methods,
+# pylint: disable=missing-function-docstring, missing-module-docstring,
+
+from conan import ConanFile  # pylint: disable=import-error
+
+
+class ProjectName(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        dependencies = [
+            "fmt/10.2.1",
+        ]
+
+        for dep in dependencies:
+            self.requires(dep)
